f513381ff1b038b6f9f8e901a1034b86d844529e
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-04-25 16:25:14 +0800

    input half way done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-color': '#04151D',
+        "custom-color": "#04151D",
+        "login-color": "#e9ebe9",
+        "wavez-color": "#07405b",
+        "input-color": "rgb(33, 49, 60)",
+        "input-bg-color": "rgb(255, 255, 255)",
+        "border-color": "rgb(124, 138, 255)"
         // custom-color for the whole website body
 
-        'custom-color-sidebar-brown': '#9A7F69',
+        "custom-color-sidebar-brown": "#9A7F69",
       },
       fontFamily: {
-        'alata': ['Alata', 'sans-serif'],
+        alata: ["Alata", "sans-serif"],
+      },
+      width: {
+        "container-width": "800px",
+        "image-width": "55%",
+        "login-width": "45%",
+      },
+      height: {
+        "login-height": "500px",
       },
     },
   },
-  plugins: [
-    require('tailwindcss-filters'),
-  ],
-}
+  plugins: [require("tailwindcss-filters")],
+};
