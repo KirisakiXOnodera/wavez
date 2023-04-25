e2a54132a5b7182fac9c5da9ea5f146154c10114
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-04-26 00:11:59 +0800

    login-page done not fully responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        "login-font-size": "40px",
+      },
+
       colors: {
         "custom-color": "#04151D",
         "login-color": "#e9ebe9",
         "wavez-color": "#07405b",
         "input-color": "rgb(33, 49, 60)",
         "input-bg-color": "rgb(255, 255, 255)",
-        "border-color": "rgb(124, 138, 255)"
+        "border-color": "rgb(124, 138, 255)",
+        "button-color": "rgba(154, 127, 105, 0.9)",
         // custom-color for the whole website body
 
         "custom-color-sidebar-brown": "#9A7F69",
